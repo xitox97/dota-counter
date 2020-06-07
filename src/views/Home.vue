01a7fe8415cd17442d23295058bbff52d573cba4
Farhan Hadi <farhan.abdhadi@gmail.com>
2020-06-07 19:03:17 +0800

    Add simple search hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="bg-white w-full h-screen container mx-auto p-6">
-      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-3/6 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" placeholder="Search Hero">
+  <div class="">
+     <h2 class="text-2xl font-bold leading-7 text-red-500 sm:text-3xl sm:leading-9 sm:truncate mb-2">
+      Dota 2 Counter pick
+    </h2>
+      <SearchHero></SearchHero>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import SearchHero from '@/components/SearchHero.vue'
 
 export default {
   name: 'Home',
   components: {
+    SearchHero
   }
 }
 </script>
