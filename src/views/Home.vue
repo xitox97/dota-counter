d790a1308275fd1666548a96823473fb2af279df
Farhan Hadi <farhan.abdhadi@gmail.com>
2020-06-07 17:34:10 +0800

    Add tailwind css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Farhan Hadi was Here"/>
+  <div class="bg-white w-full h-screen container mx-auto p-6">
+      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-3/6 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" placeholder="Search Hero">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
