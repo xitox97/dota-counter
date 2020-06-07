58f24dbacfaba8b29580857aa20034ee55c603ec
Farhan Hadi <farhan.abdhadi@gmail.com>
2020-06-07 23:49:35 +0800

    Add good against draft

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,24 @@
         <img class="inline-block h-14 w-auto rounded-lg" :src="'http://cdn.dota2.com' + hero.img" alt="" />
         <p class="ml-2 text-white leading-6 font-semibold mt-2">{{ hero.localized_name }}</p>
       </div>
-
+    </div>
+    <div class="flex flex-col items-start">
+      <div>
+        <h2 class="text-sm font-semibold leading-7 text-green-600 sm:text-3xl sm:leading-9 sm:truncate mb-8">
+          Good against
+        </h2>
+      </div>
+      <div class="my-1 flex text-white space-y-2 flex-wrap">
+        <div v-for="(hero, index) in results" :key="hero.hero_id">
+          <span v-if="index < 15">
+            <img class="inline-block h-12 w-auto rounded-lg" :src="'http://cdn.dota2.com' + heroes[hero.hero_id].img" alt="" />
+            <div>
+              <span class="ml-2 text-white leading-6 font-semibold mr-2">{{ heroes[hero.hero_id].localized_name }}</span>
+              <span class="ml-2 text-white leading-6 font-semibold mr-2">{{ hero.percents }}</span>
+            </div>
+          </span>
+        </div>
+      </div>
     </div>
 
     <button class="bg-transparent hover:bg-red-500 text-red-400 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
@@ -29,35 +46,45 @@
       return {
         heroesId: this.$route.params.id,
         heroes: heroes,
+        results: null,
       };
     },
     computed: {
       hero() {
         var items = this.heroes;
         var result = Object.keys(items)
-          .map(key => items[key]) // turn an array of keys into array of items.
-          .filter(item => item.id == this.heroesId) // filter that array,
+          .map(key => items[key])
+          .filter(item => item.id == this.heroesId);
         return result[0];
       }
     },
     created() {
       axios.get(`https://api.opendota.com/api/heroes/` + this.heroesId + `/matchups`)
         .then(response => {
-          // JSON responses are automatically parsed.
-          console.log('masuk');
           var matchup = response.data;
-           var result = {}
-          // var result = Object.keys(matchup)
-          //           .map(key => matchup[key].wins / matchup[key].games_played * 100) // turn an array of keys into array of items.
+          var result = {};
+
           Object.keys(matchup).forEach(key => {
-            const item = matchup[key]
-            if(item.games_played > 99){
-            var percents = item.wins / matchup[key].games_played * 100;
-            item["percents"] = percents.toFixed(2);
-            result[key] = item
+            const item = matchup[key];
+            if (item.games_played > 99) {
+              var percents = item.wins / matchup[key].games_played * 100;
+              if (percents > 50) {
+                item["percents"] = percents.toFixed(2);
+                result[key] = item;
+              }
             }
           })
-          console.log(result)
+          this.results = result;
+          // var finalResult = [];
+          // Object.keys(this.results).forEach(key => {
+          //   finalResult.push(Object.keys(this.heroes)
+          //   .map(key => this.heroes[key])
+          //   .filter(item => item.id == this.results[key]['hero_id']));
+          // });
+
+          console.log(this.heroes[3]);
+
+          //console.log(this.heroes[1]);
         })
         .catch(e => {
           //this.errors.push(e)
